a4b991fba64354961fde4ea354c687cf7702a75b
ale tzo <ajo@mydevnull.net>
2011-12-28 23:13:21 +0200

    added a default symfony 1.4 structure

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/Applications/MAMP/bin/symfony/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
