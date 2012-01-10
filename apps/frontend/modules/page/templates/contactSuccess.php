57a6b790b59d8f5468f4ab97d79394be77db20e1
ale tzo <ajo@mydevnull.net>
2012-01-10 16:18:20 +0200

    added the about and the contact pages

--- apps/frontend/modules/page/templates/contactSuccess.php
+++ apps/frontend/modules/page/templates/contactSuccess.php
@@ -1,1 +1,5 @@
 <?php use_helper('I18N') ?>
+
+<p><?php echo __('You may reach me at ajo at my-dev-null dot net (remove the dashes and make some appropriate convertions to get an email address).') ?></p>
+
+<p><?php echo __('If the above is too complicated for you, try to tweet me at aletzo. But, there is no guarantee that I\'ll notice. Not a frequent user.') ?></p>
